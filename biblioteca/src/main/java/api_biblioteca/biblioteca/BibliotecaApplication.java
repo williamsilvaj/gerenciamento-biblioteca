e4e49ba1533be82e9f576682ce631efd9a917bed
Will1929 <119798187+Will1929@users.noreply.github.com>
2023-05-03 15:29:53 -0300

    Add files via upload

---
+++ biblioteca/src/main/java/api_biblioteca/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,16 @@
+package api_biblioteca.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+		System.out.println("Fonis");
+	}
+
+}
---
+++ biblioteca/src/main/java/api_biblioteca/biblioteca/controle/Controle.java
@@ -0,0 +1,8 @@
+package api_biblioteca.biblioteca.controle;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controle {
+    
+}
